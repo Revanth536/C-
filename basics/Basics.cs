e7d65ecc723e5d28b1ce17d994b6dd66dc040a0f
Revanth536 <133359568+Revanth536@users.noreply.github.com>
2026-02-12 10:47:56 +0530

    First Commit

---
+++ basics/Basics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics.basics
+{
+    internal class Basics
+    {
+        public static void Main(string[] args)
+        {
+            int n = 10;
+            int i = 20;
+            Console.WriteLine(n + i);
+        }
+    }
+}
