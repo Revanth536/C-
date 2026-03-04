4ad709608facde53ffe0c5de5d3c104df1f37863
Revanth536 <133359568+Revanth536@users.noreply.github.com>
2026-03-04 11:01:55 +0530

    Create Arr class with Main method for array input
    Fix syntax errors and improve input handling.

---
+++ basics/Ar.cs
@@ -0,0 +1,22 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics.basics
+{
+    internal class Arr
+    {
+        public static void Main(String[] args)
+        {
+           in int n=Convert.ToInt32(Console.ReadLine());
+ int[] a = new int[n];
+ for (int i = 0; i < n; i++) 
+         a[i] = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine(a);t n=ConvertToInt32(console.Read());
+            
+        }
+    }
+}
